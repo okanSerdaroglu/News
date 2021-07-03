e23b6a3533d5954ad46443dfd7cd25f72fe9ba08
okanSerdaroglu <okanserdaroglu2@gmail.com>
2021-07-04 00:22:03 +0300

    call mapper test refactor

--- app/src/test/java/com/example/news/data/remote/NewsCallMapperTest.kt
+++ app/src/test/java/com/example/news/data/remote/NewsCallMapperTest.kt
@@ -1,23 +1,25 @@
 package com.example.news.data.remote
 
 import com.example.news.SharedModel
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.*
 import org.junit.Test
 
-class MewsCallMapperTest {
+class NewsCallMapperTest {
+
     private val newsCallMapper = NewsCallMapper()
 
     @Test
     fun `map from entity success`() {
         val callNews = SharedModel.callNews
         val mappedNews = newsCallMapper.mapFromEntity(callNews)
-        Truth.assertThat(mappedNews).isEqualTo(SharedModel.news)
+        assertThat(mappedNews).isEqualTo(SharedModel.news)
+
     }
 
     @Test
     fun `map to entity success`() {
         val news = SharedModel.news
         val mappedNews = newsCallMapper.mapToEntity(news)
-        Truth.assertThat(mappedNews).isEqualTo(SharedModel.callNews)
+        assertThat(mappedNews).isEqualTo(SharedModel.callNews)
     }
 }
