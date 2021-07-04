a057fd66f640b32700490cb19fee8c0473f2d3cc
okanSerdaroglu <okanserdaroglu2@gmail.com>
2021-07-04 17:40:36 +0300

    headLines API methods and headLinesResponse

--- app/src/main/java/com/example/news/data/remote/NewsAPI.kt
+++ app/src/main/java/com/example/news/data/remote/NewsAPI.kt
@@ -1,12 +1,20 @@
 package com.example.news.data.remote
 
+import com.example.news.data.remote.headlines.HeadLinesResponse
 import com.example.news.data.remote.news.NewsSourcesResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface NewsAPI {
 
     @GET("v2/sources")
     suspend fun getNewsHeaders(): Response<NewsSourcesResponse>
+
+    @GET("v2/top-headlines")
+    suspend fun getAllHeadLines(
+        @Query("page") page: Int,
+        @Query("pageSize") pageSize: Int
+    ) : Response<HeadLinesResponse>
 
 }
